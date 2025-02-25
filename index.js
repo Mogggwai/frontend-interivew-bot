dc39e1ba4dc785515e47ae70c4bd427e724082d6
Mogggwai <Moggwai4815@keemail.me>
2025-02-25 12:04:39 +0500

    Добавлена обработка команды /start

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ require('dotenv').config({ path: './src/.env' });
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
+// /start
+bot.command('start', async (ctx) => {
+    await ctx.reply(
+        'Привет! Я помогу тебе подготовиться к собеседованию на позицию фронтенд-разработчика! Выбери тему:'
+    );
+    });
+
 bot.start();
